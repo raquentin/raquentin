4c543fab86cde4507769945ade361ff75c4d4e0b
r4c3 <63271957+r4c3@users.noreply.github.com>
2022-08-08 19:00:44 -0400

    def setup
    asd

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,14 @@
+<script>
+
+
+</script>
+
+
+<style lang="scss">
+    :global(:root) {
+        --green: #293d3a;
+        --pink: #ff7965;
+        --white: #ffffff;
+        --grey: #222222;
+    }
+</style>
