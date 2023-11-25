a0242486773f69d7876e4d219a7ef6409bbc27dd
r4c3 <rqwilliams9@gmail.com>
2023-11-25 14:02:42 -0500

    done

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,62 @@
 <script>
-
 </script>
 
 
 <div id="content-container">
-    <h1 id="name-text">Race Williams</h1>
+    <div class="transition" id="madvillainy-square" />
+    <h1 class="bottom-margin" id="name-text">Race Williams</h1>
     <div id="text-container">
         <p class="body-text">I’m a 6.39π year-old software engineer from the US.</p>
         <p class="body-text">Current work:</p>
-            <p class="body-text">&nbsp;&nbsp;• SWE @ Blonde, a MMS-based social media network.</p>
-            <p class="body-text">&nbsp;&nbsp;• BSCS @ Georgia Tech, studying HCI and audio programming.</p>
+            <p class="body-text">&nbsp;&nbsp;• SWE @ Blonde, an MMS-based social media network.</p>
+            <p class="body-text">&nbsp;&nbsp;• CS @ Georgia Tech, studying HCI and audio programming.</p>
             <p class="body-text">&nbsp;&nbsp;• Trainsient, a CNN-powered transient shaper.</p>
     </div>
-    <p class="body-text vertical-margin">See GitHub for source code and downloads. Одна любовь.</p>
+    <p class="body-text bottom-margin">See GitHub for source code and downloads. Одна любовь.</p>
     <div id="socials-container">
-        <a href="mailto:race@racewilliams.com" class="delink"><p class="body-text inline-link">race@racewilliams.com</p></a>
+        <a href="https://github.com/r4c3" target="_blank" rel="noopener noreferrer">
+            <svg viewBox="0 0 50 44" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <g clip-path="url(#clip0_8_32)">
+                <path d="M33.3499 25.4558C31.4544 25.4558 29.9179 27.5863 29.9179 30.2052C29.9179 32.8293 31.4544 34.9598 33.3499 34.9598C35.2455 34.9598 36.7819 32.8293 36.7819 30.2052C36.7793 27.5863 35.2455 25.4558 33.3499 25.4558ZM45.2986 13.0927C45.692 12.1291 45.7078 6.6537 43.6222 1.40802C43.6222 1.40802 38.8412 1.93338 31.6075 6.89922C30.0896 6.47682 27.5208 6.2709 24.9574 6.2709C22.3887 6.2709 19.8226 6.47682 18.3046 6.89922C11.0683 1.93338 6.28731 1.40802 6.28731 1.40802C4.20435 6.6537 4.22547 12.1291 4.61355 13.0927C2.16363 15.7538 0.666748 18.9508 0.666748 23.3121C0.666748 42.2832 16.4091 42.5868 20.3796 42.5868L24.9574 42.592L29.5299 42.5868C33.5057 42.5868 49.2428 42.2832 49.2428 23.3121C49.2428 18.9508 47.7485 15.7538 45.2986 13.0927ZM25.0287 40.2556H24.8808C14.9254 40.2556 7.16907 39.0703 7.16907 29.392C7.16907 27.0715 7.98747 24.9225 9.93315 23.1379C13.1724 20.1626 18.6583 21.736 24.8808 21.736H25.0287C31.2511 21.736 36.7397 20.1626 39.9816 23.1379C41.922 24.9225 42.7404 27.0715 42.7404 29.392C42.7404 39.0703 34.9867 40.2556 25.0287 40.2556ZM16.5622 25.4558C14.6667 25.4558 13.1302 27.5863 13.1302 30.2052C13.1302 32.8293 14.6667 34.9598 16.5622 34.9598C18.4603 34.9598 19.9968 32.8293 19.9968 30.2052C19.9968 27.5863 18.4603 25.4558 16.5622 25.4558Z" fill="white"/>
+                </g>
+                <defs>
+                <clipPath id="clip0_8_32">
+                <rect width="49.8667" height="44" fill="white" transform="translate(0.0214844)"/>
+                </clipPath>
+                </defs>
+            </svg>
+        </a>     
+        <a href="https://linkedin.com/in/r4c3" target="_blank" rel="noopener noreferrer">
+            <svg viewBox="0 0 45 44" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <g clip-path="url(#clip0_8_29)">
+                <path fill-rule="evenodd" clip-rule="evenodd" d="M10.0336 42.9875V14.6656H0.620899V42.9874H10.0336V42.9875ZM5.32821 10.8001C8.60957 10.8001 10.6536 8.62347 10.6536 5.90569C10.5915 3.12751 8.60957 1.01233 5.38975 1.01233C2.16941 1.01242 0.0644531 3.12759 0.0644531 5.90578C0.0644531 8.62355 2.10736 10.8002 5.26625 10.8002L5.32821 10.8001ZM15.2431 42.9875C15.2431 42.9875 15.3666 17.3229 15.2431 14.6656H24.6572V18.7729H24.5947C25.8326 16.8402 28.0626 14.0006 33.1404 14.0006C39.3352 14.0006 43.9785 18.0485 43.9785 26.7479V42.9876H34.5659V27.8362C34.5659 24.029 33.2039 21.4312 29.7966 21.4312C27.1967 21.4312 25.6472 23.1826 24.9667 24.8755C24.7178 25.4781 24.6572 26.3245 24.6572 27.171V42.9875H15.2431Z" fill="white"/>
+                </g>
+                <defs>
+                <clipPath id="clip0_8_29">
+                <rect width="44" height="44" fill="white" transform="translate(0.0214844)"/>
+                </clipPath>
+                </defs>
+            </svg>
+        </a>
+        <a href="https://stackoverflow.com/users/20668816/race-williams" target="_blank" rel="noopener noreferrer">
+            <svg viewBox="0 0 39 44" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M31.8256 39.421V27.8128H36.1798V43.7773H1.34644V27.8128H5.7006V39.4232L31.8256 39.421ZM9.18175 27.9565L9.86318 23.6458L29.1021 27.3469L28.856 30.7997L9.18175 27.9565ZM11.7943 18.0899L13.3879 14.6066L30.8002 22.7337L29.2044 26.2148L11.7943 18.0899ZM16.7253 9.52964L19.192 6.62759L33.9918 18.9608L31.5251 21.8628L16.7253 9.52964ZM26.1565 0.533936L37.6188 15.9129L34.5709 18.2358L23.1107 2.85471L26.1586 0.533936H26.1565ZM8.60047 36.5168V32.1626H28.9127V36.5168H8.60047Z" fill="white"/>
+            </svg>
+        </a>
+        <a href="https://www.discogs.com/user/fatlats68/collection" target="_blank" rel="noopener noreferrer">
+            <svg viewBox="0 0 45 44" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <g clip-path="url(#clip0_8_37)">
+                <path d="M22.0132 24.8769C23.602 24.8769 24.8901 23.5889 24.8901 22C24.8901 20.4111 23.602 19.123 22.0132 19.123C20.4243 19.123 19.1362 20.4111 19.1362 22C19.1362 23.5889 20.4243 24.8769 22.0132 24.8769Z" fill="white"/>
+                <path d="M22.0132 0C9.86297 0 0.0131836 9.84978 0.0131836 22C0.0131836 34.1502 9.86297 44 22.0132 44C34.1634 44 44.0132 34.1502 44.0132 22C44.0132 9.84978 34.1634 0 22.0132 0ZM13.6249 5.47157C16.1432 4.19135 18.9939 3.46933 22.0132 3.46933C22.1905 3.46933 22.3662 3.47172 22.542 3.47649C22.8338 3.48444 23.0342 4.20964 22.9904 5.08671L22.7662 9.6009C22.7225 10.478 22.5364 11.1801 22.3503 11.1745C22.2382 11.1714 22.1261 11.1698 22.0132 11.1698C20.4252 11.1698 18.9168 11.5117 17.5578 12.1256C16.7571 12.4874 15.7989 12.2719 15.352 11.5157L13.0492 7.62172C12.6023 6.86551 12.8417 5.86995 13.6249 5.47157ZM12.7248 13.7525L9.32305 10.777C8.66146 10.1989 8.59308 9.19138 9.22842 8.58546C9.24353 8.57115 9.25784 8.55763 9.27375 8.54332C9.91147 7.93899 10.911 8.06224 11.4501 8.75483L14.2285 12.3204C14.7684 13.013 14.894 13.8225 14.5394 14.1604C14.1839 14.4999 13.3864 14.3306 12.7248 13.7525ZM14.2285 22C14.2285 17.7005 17.7137 14.2153 22.0132 14.2153C26.3127 14.2153 29.7979 17.7005 29.7979 22C29.7979 26.2995 26.3127 29.7847 22.0132 29.7847C17.7137 29.7847 14.2285 26.2995 14.2285 22ZM30.3649 38.5467C27.8553 39.8158 25.0173 40.5307 22.0132 40.5307C21.816 40.5307 21.6196 40.5275 21.424 40.5219C21.0987 40.5116 20.8737 39.7832 20.9214 38.9061L21.1679 34.3927C21.2164 33.5157 21.424 32.8175 21.6339 32.8247C21.7595 32.8286 21.886 32.831 22.0132 32.831C23.4715 32.831 24.8615 32.5432 26.1314 32.0215C26.944 31.6876 27.9404 31.907 28.4191 32.6434L30.8849 36.4363C31.3636 37.1719 31.1489 38.1499 30.3649 38.5467ZM34.6978 35.5092C34.0887 36.0817 33.1034 35.9473 32.5484 35.2666L29.6937 31.7623C29.1387 31.0816 28.9972 30.2928 29.3478 29.9692C29.6993 29.6456 30.4953 29.8269 31.1561 30.4058L34.5594 33.3829C35.221 33.961 35.3076 34.9366 34.6978 35.5092Z" fill="white"/>
+                </g>
+                <defs>
+                <clipPath id="clip0_8_37">
+                <rect width="44" height="44" fill="white" transform="translate(0.0131836)"/>
+                </clipPath>
+                </defs>
+            </svg>
+        </a>
+        <a href="mailto:race@racewilliams.com" class="delink" target="_blank" rel="noopener noreferrer"><p class="body-text inline-link transition">race@racewilliams.com</p></a>
     </div>
 </div>
 
@@ -24,34 +66,106 @@
         display: flex;
         flex-direction: column;
         width: 66vw;
-        transform: translateY(-8vh);
+        transform: translateY(-4vh);
+    }
+    #madvillainy-square {
+        width: 2vw;
+        height: 2vw;
+        position: absolute;
+        top: 1vh;
+        right: 0;
+    }
+    #madvillainy-square:hover {
+        background-color: #EA5923;
     }
     #name-text {
         font-weight: 800;
         font-size: 3.4vw;
+        margin: 0 0 2vh 0;
     }
     #text-container {
         display: flex;
         flex-direction: column;
+        margin-bottom: 3vh;
     }
     #socials-container {
         display: flex;
         gap: 2vw;
         align-items: center;
     }
+    #socials-container svg {
+        height: 2.6vw;
+    }
+    #socials-container svg path {
+        transition: 0.3s ease all;
+    }
+    #socials-container svg:hover path {
+        fill: #EA5923;
+    }
     .body-text {
         font-size: 2vw;
         line-height: 1;
         margin: 0.5vh;
     }
-    .vertical-margin {
-        margin: 4vh 0vw;
+    .bottom-margin {
+        margin-bottom: 4vh;
     }
     .inline-link {
         font-weight: 600;
     }
+    .inline-link:hover {
+        color: #EA5923;
+    }
     .delink {
         color: inherit;
         text-decoration: none;
+    }
+    .transition {
+        transition: 0.2s ease fill;
+        transition: 0.2s ease color;
+        transition: 0.2s ease background-color;
+    }
+
+
+    /* For tablets (iPad) */
+    @media screen and (max-width: 768px) {
+        #content-container {
+            width: 72vw; /* Adjust the width */
+        }
+        #name-text {
+            font-size: 5vw; /* Larger font size for smaller screens */
+        }
+        .body-text {
+            font-size: 2.3vw;
+        }
+        .delink {
+            font-size: 2.8vw;
+        }
+        /* Add more styles specific for tablet displays */
+    }
+
+    /* For phones */
+    @media screen and (max-width: 480px) {
+        #content-container {
+            width: 88vw; /* Adjust the width */
+        }
+        #name-text {
+            font-size: 7vw; /* Even larger font size for smaller screens */
+        }
+        .body-text {
+            margin: 0.2vh;
+            font-size: 3vw;
+        }
+        .delink {
+            font-size: 4vw;
+        }
+        #socials-container {
+            margin-top: 2vh;
+            gap: 4vw;
+        }
+        #socials-container svg {
+            height: 6vw;
+        }
+        /* Add more styles specific for phone displays */
     }
 </style>
