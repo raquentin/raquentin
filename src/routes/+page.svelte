40309ca76fce4247f185b928f205d0d682cad25d
r4c3 <rqwilliams9@gmail.com>
2023-11-25 01:12:49 -0500

    init

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,57 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+
+</script>
+
+
+<div id="content-container">
+    <h1 id="name-text">Race Williams</h1>
+    <div id="text-container">
+        <p class="body-text">I’m a 6.39π year-old software engineer from the US.</p>
+        <p class="body-text">Current work:</p>
+            <p class="body-text">&nbsp;&nbsp;• SWE @ Blonde, a MMS-based social media network.</p>
+            <p class="body-text">&nbsp;&nbsp;• BSCS @ Georgia Tech, studying HCI and audio programming.</p>
+            <p class="body-text">&nbsp;&nbsp;• Trainsient, a CNN-powered transient shaper.</p>
+    </div>
+    <p class="body-text vertical-margin">See GitHub for source code and downloads. Одна любовь.</p>
+    <div id="socials-container">
+        <a href="mailto:race@racewilliams.com" class="delink"><p class="body-text inline-link">race@racewilliams.com</p></a>
+    </div>
+</div>
+
+
+<style>
+    #content-container {
+        display: flex;
+        flex-direction: column;
+        width: 66vw;
+        transform: translateY(-8vh);
+    }
+    #name-text {
+        font-weight: 800;
+        font-size: 3.4vw;
+    }
+    #text-container {
+        display: flex;
+        flex-direction: column;
+    }
+    #socials-container {
+        display: flex;
+        gap: 2vw;
+        align-items: center;
+    }
+    .body-text {
+        font-size: 2vw;
+        line-height: 1;
+        margin: 0.5vh;
+    }
+    .vertical-margin {
+        margin: 4vh 0vw;
+    }
+    .inline-link {
+        font-weight: 600;
+    }
+    .delink {
+        color: inherit;
+        text-decoration: none;
+    }
+</style>
