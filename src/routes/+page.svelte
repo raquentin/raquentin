8b111cea44404281318a3a009e612694190ca8c7
r4c3 <race@racewilliams.com>
2024-01-02 21:27:57 -0500

    update shi

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <script>
-    const yearsInPi = ((new Date() - new Date('2003-10-30')) / (1000 * 60 * 60 * 24 * 365.25) / Math.PI).toFixed(2);
+    const yearsInPi = ((new Date() - new Date('2003-10-30')) / (1000 * 60 * 60 * 24 * 365.25)).toFixed(0);
 </script>
 
 
@@ -7,13 +7,13 @@
     <div class="transition" id="madvillainy-square" />
     <h1 class="bottom-margin" id="name-text">Race Williams</h1>
     <div id="text-container">
-        <p class="body-text">I’m a {yearsInPi}π year-old software engineer from the US.</p>
-        <p class="body-text">Current work:</p>
-            <p class="body-text">&nbsp;&nbsp;• SWE @ Blonde, an MMS-based social media network.</p>
-            <p class="body-text">&nbsp;&nbsp;• CS @ Georgia Tech, studying HCI and audio programming.</p>
-            <p class="body-text">&nbsp;&nbsp;• Trainsient, a CNN-powered transient shaper.</p>
+        <p class="body-text">I’m a {yearsInPi} year-old software engineer from the US.</p>
+        <p class="body-text">Currently working on:</p>
+            <p class="body-text">&nbsp;&nbsp;• Blonde, a p2p social media network built on MMS.</p>
+            <p class="body-text">&nbsp;&nbsp;• BSCS @ Georgia Tech, studying algorithms, comp audio, and UX.</p>
+            <p class="body-text">&nbsp;&nbsp;• A number of open source projects.</p>
     </div>
-    <p class="body-text bottom-margin">See GitHub for source code and downloads. Одна любовь.</p>
+    <p class="body-text bottom-margin">See GitHub for source code and downloads.</p>
     <div id="socials-container">
         <a href="https://github.com/r4c3" target="_blank" rel="noopener noreferrer">
             <svg viewBox="0 0 50 44" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -44,20 +44,9 @@
                 <path d="M31.8256 39.421V27.8128H36.1798V43.7773H1.34644V27.8128H5.7006V39.4232L31.8256 39.421ZM9.18175 27.9565L9.86318 23.6458L29.1021 27.3469L28.856 30.7997L9.18175 27.9565ZM11.7943 18.0899L13.3879 14.6066L30.8002 22.7337L29.2044 26.2148L11.7943 18.0899ZM16.7253 9.52964L19.192 6.62759L33.9918 18.9608L31.5251 21.8628L16.7253 9.52964ZM26.1565 0.533936L37.6188 15.9129L34.5709 18.2358L23.1107 2.85471L26.1586 0.533936H26.1565ZM8.60047 36.5168V32.1626H28.9127V36.5168H8.60047Z" fill="white"/>
             </svg>
         </a>
-        <a href="https://www.discogs.com/user/fatlats68/collection" target="_blank" rel="noopener noreferrer">
-            <svg viewBox="0 0 45 44" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <g clip-path="url(#clip0_8_37)">
-                <path d="M22.0132 24.8769C23.602 24.8769 24.8901 23.5889 24.8901 22C24.8901 20.4111 23.602 19.123 22.0132 19.123C20.4243 19.123 19.1362 20.4111 19.1362 22C19.1362 23.5889 20.4243 24.8769 22.0132 24.8769Z" fill="white"/>
-                <path d="M22.0132 0C9.86297 0 0.0131836 9.84978 0.0131836 22C0.0131836 34.1502 9.86297 44 22.0132 44C34.1634 44 44.0132 34.1502 44.0132 22C44.0132 9.84978 34.1634 0 22.0132 0ZM13.6249 5.47157C16.1432 4.19135 18.9939 3.46933 22.0132 3.46933C22.1905 3.46933 22.3662 3.47172 22.542 3.47649C22.8338 3.48444 23.0342 4.20964 22.9904 5.08671L22.7662 9.6009C22.7225 10.478 22.5364 11.1801 22.3503 11.1745C22.2382 11.1714 22.1261 11.1698 22.0132 11.1698C20.4252 11.1698 18.9168 11.5117 17.5578 12.1256C16.7571 12.4874 15.7989 12.2719 15.352 11.5157L13.0492 7.62172C12.6023 6.86551 12.8417 5.86995 13.6249 5.47157ZM12.7248 13.7525L9.32305 10.777C8.66146 10.1989 8.59308 9.19138 9.22842 8.58546C9.24353 8.57115 9.25784 8.55763 9.27375 8.54332C9.91147 7.93899 10.911 8.06224 11.4501 8.75483L14.2285 12.3204C14.7684 13.013 14.894 13.8225 14.5394 14.1604C14.1839 14.4999 13.3864 14.3306 12.7248 13.7525ZM14.2285 22C14.2285 17.7005 17.7137 14.2153 22.0132 14.2153C26.3127 14.2153 29.7979 17.7005 29.7979 22C29.7979 26.2995 26.3127 29.7847 22.0132 29.7847C17.7137 29.7847 14.2285 26.2995 14.2285 22ZM30.3649 38.5467C27.8553 39.8158 25.0173 40.5307 22.0132 40.5307C21.816 40.5307 21.6196 40.5275 21.424 40.5219C21.0987 40.5116 20.8737 39.7832 20.9214 38.9061L21.1679 34.3927C21.2164 33.5157 21.424 32.8175 21.6339 32.8247C21.7595 32.8286 21.886 32.831 22.0132 32.831C23.4715 32.831 24.8615 32.5432 26.1314 32.0215C26.944 31.6876 27.9404 31.907 28.4191 32.6434L30.8849 36.4363C31.3636 37.1719 31.1489 38.1499 30.3649 38.5467ZM34.6978 35.5092C34.0887 36.0817 33.1034 35.9473 32.5484 35.2666L29.6937 31.7623C29.1387 31.0816 28.9972 30.2928 29.3478 29.9692C29.6993 29.6456 30.4953 29.8269 31.1561 30.4058L34.5594 33.3829C35.221 33.961 35.3076 34.9366 34.6978 35.5092Z" fill="white"/>
-                </g>
-                <defs>
-                <clipPath id="clip0_8_37">
-                <rect width="44" height="44" fill="white" transform="translate(0.0131836)"/>
-                </clipPath>
-                </defs>
-            </svg>
+        <a href="mailto:race@racewilliams.com" class="delink" target="_blank" rel="noopener noreferrer">
+          <p class="body-text inline-link">race@racewilliams.com</p>
         </a>
-        <a href="mailto:race@racewilliams.com" class="delink" target="_blank" rel="noopener noreferrer"><p class="body-text inline-link transition">race@racewilliams.com</p></a>
     </div>
 </div>
 
@@ -113,6 +102,7 @@
     }
     .inline-link {
         font-weight: 600;
+        transition: all 0.2s ease;
     }
     .inline-link:hover {
         color: #EA5923;
@@ -120,6 +110,7 @@
     .delink {
         color: inherit;
         text-decoration: none;
+        transition: 0.2 ease all;
     }
     .transition {
         transition: 0.2s ease fill;
@@ -142,7 +133,6 @@
         .delink {
             font-size: 2.8vw;
         }
-        /* Add more styles specific for tablet displays */
     }
 
     /* For phones */
@@ -167,6 +157,5 @@
         #socials-container svg {
             height: 6vw;
         }
-        /* Add more styles specific for phone displays */
     }
 </style>
